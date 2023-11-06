61b6619a36ee14b9412eeda8aac3483b83dcb0b6
betabravah <betabravah@gmail.com>
2023-11-06 21:31:05 +0300

    fix: Remove Filter Items Usecase

--- trove/lib/features/item/data/datasources/local/local_datasource.dart
+++ trove/lib/features/item/data/datasources/local/local_datasource.dart
@@ -4,9 +4,7 @@ import 'package:trove/features/item/data/models/item_model.dart';
 abstract class ItemLocalDataSource {
   Future<List<ItemModel>> getItems();
   Future<ItemModel> getItem(String id);
-  Future<ItemModel> deleteItem(String id);
-
-  Future<List<ItemModel>> filterItems(String title);
+  Future<void> deleteItem(String id);
 
   Future<List<ItemModel>> getLastItems();
   Future<void> cacheItems(List<ItemModel> items);
