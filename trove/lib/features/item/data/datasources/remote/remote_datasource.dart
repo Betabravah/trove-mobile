38539a222e534a64ffd258ab89e0fffc39e12f04
betabravah <betabravah@gmail.com>
2023-11-07 23:10:04 +0300

    fix: Make Return of DeleteItem ItemModel

--- trove/lib/features/item/data/datasources/remote/remote_datasource.dart
+++ trove/lib/features/item/data/datasources/remote/remote_datasource.dart
@@ -1,11 +1,11 @@
 import 'package:trove/features/item/data/models/item_model.dart';
 
-abstract class RemoteDatasource {
+abstract class ItemRemoteDataSource {
   Future<List<ItemModel>> getItems();
   Future<ItemModel> getItem(String id);
   Future<ItemModel> createItem(ItemModel item);
   Future<ItemModel> updateItem(ItemModel item);
-  Future<void> deleteItem(String id);
+  Future<ItemModel> deleteItem(String id);
 
   Future<List<ItemModel>> filterItems(String query);
 }
